3122767e72d7b57c862828aac2e108419165e27e
sundantist <workspace.sundantist@gmail.com>
2024-04-16 20:39:25 +0300

    Теперь оно норм выглядит :/ хуй

--- second-branch/second_script_parce.py
+++ second-branch/second_script_parce.py
@@ -17,9 +17,9 @@ driver = webdriver.Chrome(options=options)
 driver.get(url)
 content = driver.page_source
 soup = BeautifulSoup(content, 'lxml')
-
-
 all_thread_link = soup.find_all(attrs={"class" : "post__reflink"})
+
+
 
 # Запись html страницы в файл для удобства отладки
 html_file = open("html.index", "w+", encoding="utf-8")
@@ -45,13 +45,15 @@ html_file.close()
 
 def get_number_of_thread():
 # Получение линков на треды, но тут еще прочее говно
-
+    thread_list = []
+    thread_list_sort = []
     for item in all_thread_link:
         item_link = item.get("href")
+        print(item_link) #################################################################
         if item_link not in thread_list:
             symbols_to_remove = "." # Убираем точки из строки
             for symbol in symbols_to_remove:
-                item_link = item_link.replace(symbol, "")
+                item_link = item_link.replace(symbol, '')
             thread_list.append('2ch.hk' + item_link[0:13]) # Делаем срез для удобства будущей сортировки 
 # Уничтожение повторяющихся позиций в списке
     for num in thread_list:
@@ -63,15 +65,13 @@ def get_number_of_thread():
 def scroll():
     last_height = driver.execute_script("return document.body.scrollHeight")
     while True:
-        get_number_of_thread
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(4)  # Необходимо дать время для загрузки страницы через AJAX.
+        time.sleep(1)  # Необходимо дать время для загрузки страницы через AJAX.
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             break  # Если высота страницы не меняется, это означает, что прокрутка достигла низа.
         last_height = new_height
-    return get_number_of_thread()
 
 #--------------------------------------------------------------------------------------------------------- Отладка
-
-print(scroll())
+scroll()
+print(get_number_of_thread())
